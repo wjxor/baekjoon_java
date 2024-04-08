c6904949e6d6aef9399ad5e6389d0051c6d2ef18
wjxor <shko940575@gmail.com>
2024-04-08 20:56:32 +0900

    10809

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,13 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+import java.util.Scanner;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("hi");
+        Scanner sc = new Scanner(System.in);
+
+        String s = sc.nextLine();
+
+        for (char c = 'a'; c <= 'z'; c++) {
+            System.out.print(s.indexOf(c) + " ");
+        }
     }
 }
