d9d1c72daed50a7a4052acb83113029113555c13
wjxor <shko940575@gmail.com>
2024-04-08 22:30:45 +0900

    2675

--- src/Main.java
+++ src/Main.java
@@ -4,10 +4,26 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        String s = sc.nextLine();
+        int T = sc.nextInt();
 
-        for (char c = 'a'; c <= 'z'; c++) {
-            System.out.print(s.indexOf(c) + " ");
+        for (int i = 0; i < T; i++) {
+            int R = sc.nextInt();
+            String S = sc.next();
+
+            // 결과 문자열을 저장할 변수
+            StringBuilder result = new StringBuilder();
+
+            // 문자열 S의 각 문자에 대해 R번 반복하여 result에 추가
+            for (char c : S.toCharArray()) {
+                for (int j = 0; j < R; j++) {
+                    result.append(c);
+                }
+            }
+
+            System.out.println(result);
+
         }
+
+        sc.close();
     }
 }
