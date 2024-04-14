825693e5ba875558506cbe4eb78b646240afc42d
wjxor <shko940575@gmail.com>
2024-04-14 19:55:42 +0900

    1152

--- src/Main.java
+++ src/Main.java
@@ -1,28 +1,15 @@
 import java.util.Scanner;
+import java.util.StringTokenizer;
 
 public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        int T = sc.nextInt();
+        String str = sc.nextLine().trim();
 
-        for (int i = 0; i < T; i++) {
-            int R = sc.nextInt();
-            String S = sc.next();
+        StringTokenizer st = new StringTokenizer(str, " ");
 
-            // 결과 문자열을 저장할 변수
-            StringBuilder result = new StringBuilder();
-
-            // 문자열 S의 각 문자에 대해 R번 반복하여 result에 추가
-            for (char c : S.toCharArray()) {
-                for (int j = 0; j < R; j++) {
-                    result.append(c);
-                }
-            }
-
-            System.out.println(result);
-
-        }
+        System.out.println(st.countTokens());
 
         sc.close();
     }
