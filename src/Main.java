65a1208f6b526f7342edd321c1c567392c64680e
wjxor <shko940575@gmail.com>
2024-04-15 21:09:08 +0900

    5622

--- src/Main.java
+++ src/Main.java
@@ -5,36 +5,47 @@ public class Main {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
-        // 필요한 것
         /*
-            - 수 입력받을 변수 두 변수
-            - 뒤집기
-            - 크기 비교
-            - 젤 큰 수 출력
+            필요한 것
+
+            - 문자 입력을 대문자로 받기
+            - 총 시간을 저장할 변수
+            - 각 문자에 대한 다이얼 시간 계산
+            - 총 시간 출력
 
          */
 
-        int num1 = sc.nextInt();
-        int num2 = sc.nextInt();
+        String str = sc.nextLine().toUpperCase();
+        int total = 0;
 
-        int reversed1 = reverseNumber(num1);
-        int reversed2 = reverseNumber(num2);
-
-        if (reversed1 > reversed2) {
-            System.out.println(reversed1);
-        } else {
-            System.out.println(reversed2);
+        for (int i = 0; i < str.length(); i++) {
+            total += getDialTime(str.charAt(i));
         }
+
+        System.out.println(total);
 
         sc.close();
     }
 
-    private static int reverseNumber(int num) {
-        StringBuilder sb = new StringBuilder();
+    // 문자에 따라 필요한 시간을 반환하는 메소드
+    private static int getDialTime(char ch) {
+        if (ch >= 'A' && ch <= 'C')
+            return 3;
+        if (ch >= 'D' && ch <= 'F')
+            return 4;
+        if (ch >= 'G' && ch <= 'I')
+            return 5;
+        if (ch >= 'J' && ch <= 'L')
+            return 6;
+        if (ch >= 'M' && ch <= 'O')
+            return 7;
+        if (ch >= 'P' && ch <= 'S')
+            return 8;
+        if (ch >= 'T' && ch <= 'V')
+            return 9;
+        if (ch >= 'W' && ch <= 'Z')
+            return 10;
+        return 0; // 입력에 대문자 A~Z외의 다른 문자가 있을 경우를 대비
 
-        sb.append(num);
-        sb.reverse();
-
-        return Integer.parseInt(sb.toString());
     }
 }
