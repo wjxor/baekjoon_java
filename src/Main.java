1fb3a6d8e8df8b86dd2a4d1792562458a408e2ed
wjxor <shko940575@gmail.com>
2024-04-15 21:17:22 +0900

    11718

--- src/Main.java
+++ src/Main.java
@@ -8,44 +8,21 @@ public class Main {
         /*
             필요한 것
 
-            - 문자 입력을 대문자로 받기
-            - 총 시간을 저장할 변수
-            - 각 문자에 대한 다이얼 시간 계산
-            - 총 시간 출력
+            - 문자 입력 최대 100줄
+            - 입력받은 그대로 출력
 
          */
 
-        String str = sc.nextLine().toUpperCase();
-        int total = 0;
-
-        for (int i = 0; i < str.length(); i++) {
-            total += getDialTime(str.charAt(i));
+        // 입력이 더 이상 없을 때까지 계속 읽고 출력
+        while (sc.hasNextLine()) {
+            String str = sc.nextLine();
+            System.out.println(str);
         }
 
-        System.out.println(total);
+
 
         sc.close();
     }
 
-    // 문자에 따라 필요한 시간을 반환하는 메소드
-    private static int getDialTime(char ch) {
-        if (ch >= 'A' && ch <= 'C')
-            return 3;
-        if (ch >= 'D' && ch <= 'F')
-            return 4;
-        if (ch >= 'G' && ch <= 'I')
-            return 5;
-        if (ch >= 'J' && ch <= 'L')
-            return 6;
-        if (ch >= 'M' && ch <= 'O')
-            return 7;
-        if (ch >= 'P' && ch <= 'S')
-            return 8;
-        if (ch >= 'T' && ch <= 'V')
-            return 9;
-        if (ch >= 'W' && ch <= 'Z')
-            return 10;
-        return 0; // 입력에 대문자 A~Z외의 다른 문자가 있을 경우를 대비
 
-    }
 }
