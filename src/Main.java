9d586253ec02ef76783cca8c61a10ed3108967ac
wjxor <shko940575@gmail.com>
2024-04-15 21:35:41 +0900

    2444

--- src/Main.java
+++ src/Main.java
@@ -8,18 +8,46 @@ public class Main {
         /*
             필요한 것
 
-            - 문자 입력 최대 100줄
-            - 입력받은 그대로 출력
+            - 수 입력
+
+            - 첫째 줄부터 2*N-1번째 줄까지 별 출력
+
+                *        N-1개 공백
+               ***       N-2개 공백
+              *****      N-3개 공백
+             *******     N-4개 공백
+            *********    N-5개 공백
+             *******
+              *****
+               ***
+                *
+
 
          */
 
-        // 입력이 더 이상 없을 때까지 계속 읽고 출력
-        while (sc.hasNextLine()) {
-            String str = sc.nextLine();
-            System.out.println(str);
+        int N = sc.nextInt();
+
+        for (int i = 1; i <= N; i++) {
+            for (int j = 0; j < N - i; j++) {
+                System.out.print(" ");
+            }
+
+            for (int j = 0; j < 2 * i - 1; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
         }
 
+        for (int i = N - 1; i > 0; i--) {
+            for (int j = 0; j < N - i; j++) {
+                System.out.print(" ");
+            }
 
+            for (int j = 0; j < 2 * i - 1; j++) {
+                System.out.print("*");
+            }
+            System.out.println();
+        }
 
         sc.close();
     }
