badb803a85917494fdc304929c7a717311a01a6f
wjxor <shko940575@gmail.com>
2024-04-22 23:45:48 +0900

    1157 단어 공부

--- src/Main.java
+++ src/Main.java
@@ -6,32 +6,44 @@ public class Main {
         Scanner sc = new Scanner(System.in);
 
         /*
-            10988번: 팰린드롬인지 확인하기
+            1157번: 단어 공부
             필요한것
 
-            - 문자열 입력
-            - 입력받은 문자열이 뒤집은 문자열과 같은지 확인
-            - 같으면 팰린드롬이므로 1 출력, 다르면 0 출력
+            - 문자열 입력후 대문자로 변환
+            - 알파벳 26자리의 공간을 만듦
+            - 입력한 알파벳 개수 확인
+            - 값을 하나씩 가져와서 대문자 - A(65)
+            - 해당 26자리 공간에 ++
+
+            - max 초기값 -1 ==> 배열 초기값이 0이기 떄문
+            - 최대값 찾기
+            - 최대값이 1개면 그 문자 출력, 여러개면 ? 출력
          */
 
-        String str = sc.nextLine();
+        String str = sc.nextLine().toUpperCase();
 
-        String reversed = reverseStr(str);
+        int[] alphabet = new int[26];
 
-        if (str.equals(reversed))
-            System.out.println(1);
-        else
-            System.out.println(0);
+        for (int i = 0; i < str.length(); i++) {
+            int temp = str.charAt(i) - 'A';
+            alphabet[temp]++;
+        }
+
+        int max = -1;
+        char ch = '?';
+        for (int i = 0; i < alphabet.length; i++) {
+            if (alphabet[i] > max) {
+                max = alphabet[i];
+                ch = (char)(i + 'A');
+            } else if (alphabet[i] == max) {
+                ch = '?';
+            }
+        }
+
+        System.out.println(ch);
+
 
         sc.close();
-    }
-
-    private static String reverseStr(String str) {
-        StringBuilder sb = new StringBuilder(str);
-
-        sb.reverse();
-
-        return sb.toString();
     }
 
 
